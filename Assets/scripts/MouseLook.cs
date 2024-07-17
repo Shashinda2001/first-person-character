38f16fc83521f85db75bb5ac92dfd612e0e4c113
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2024-07-17 13:28:46 +0530

    complete player look

--- Assets/scripts/MouseLook.cs
+++ Assets/scripts/MouseLook.cs
@@ -7,9 +7,10 @@ public class MouseLook : MonoBehaviour
     // Start is called before the first frame update
     public float mouseSensitivly = 100f;
     public Transform playerBody;
+    float xRotation =0f;
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -18,6 +19,10 @@ public class MouseLook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivly * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivly * Time.deltaTime;
 
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
